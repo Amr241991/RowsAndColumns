0e32f079899ad4a839e572eb515114e1ca767d62
Amr Ahmed Abdulaziz Al-sarori <114907932+Amr241991@users.noreply.github.com>
2023-03-03 14:48:09 +0300

    RwsAndColumns in java

---
+++ RwsMain.java
@@ -0,0 +1,33 @@
+package rowsAndColumns;
+
+import java.util.Scanner;
+
+public class RwsMain {
+
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner scanner = new Scanner(System.in);
+
+		int columns;
+		int rows;
+		String symbol = "";
+		
+		
+		System.out.println("Enter # of rows: ");
+		rows = scanner.nextInt();
+		System.out.println("Enter # of columns: ");
+		columns = scanner.nextInt();
+		System.out.println("Enter symbol to use: ");
+		symbol = scanner.next();
+		
+		for(int i=1; i<=rows; i++) {
+			System.out.println();
+			for(int j=1; j<=columns ; j++) {
+				System.out.print(symbol);
+			};
+		};
+	}
+
+}
